aa5fc80dc75cec343d9de1562fbe21d95bc1da2d
Franklin11111 <abdulovnail6@gmail.com>
2025-07-28 10:09:24 +0400

    Initial commit

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
 from collections import deque
 
-def canUlockAll(boxes):
+def canUnlockAll(boxes):
     num_boxes = len(boxes) # 7
     unlocked_boxes = {0}
     keys_to_process = deque(boxes[0])
 
     while keys_to_process:
-        print("Keys to process: ", keys_to_process)
+        # print("Keys to process: ", keys_to_process)
         key = keys_to_process.popleft() # 1
-        print("Key: ", key)
+        # print("Key: ", key)
         if 0 <= key < num_boxes and key not in unlocked_boxes:
             unlocked_boxes.add(key)
-            print("Number of unlocked boxes: ", len(unlocked_boxes))
-            print("Unlocked boxes: ", unlocked_boxes)
+            # print("Number of unlocked boxes: ", len(unlocked_boxes))
+            # print("Unlocked boxes: ", unlocked_boxes)
             for new_key in boxes[key]:
                 keys_to_process.append(new_key)
-        print("Updated keys to process: ", keys_to_process)
-        print("/------------------------------/")
+        # print("Updated keys to process: ", keys_to_process)
+        # print("/------------------------------/")
     return len(unlocked_boxes) == num_boxes
 
-boxes_1 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+# boxes_1 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+#
+# print(canUnlockAll(boxes_1))
 
-print(canUlockAll(boxes_1))
+# boxes = [[1], [2], [3], [4], []]
+# print(canUnlockAll(boxes))
+#
+# boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+# print(canUnlockAll(boxes))
+#
+# boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+# print(canUnlockAll(boxes))
